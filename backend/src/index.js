b2ba4a1822b36cb6cec2d5c2530587a98d6fbb76
ABHISHEK ADIGA <work.abhishek86@gmail.com>
2026-02-24 17:10:10 +0530

    Fix node version and route issue

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,16 +7,18 @@ import path from "path";
 import { app, server } from "./lib/socket.js";
 
 dotenv.config();
+
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
-const PORT = process.env.PORT;
 
+const PORT = process.env.PORT;
 const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, "/client/build")));
+
 app.use(cookieParser());
 
 app.use(express.json({ limit: "50mb" }));
+
 app.use(
   cors({
     origin: ["http://localhost:5173", "http://localhost:5174"],
@@ -26,11 +28,13 @@ app.use(
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+// ✅ Production setup
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.all("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
   });
 }
 
